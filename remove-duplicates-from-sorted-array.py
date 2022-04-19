b92a6e517c832bc4f4adf20b18ce83b10bbd1556
Katie Lee <katiejeewonlee@gmail.com>
2022-04-19 19:14:31 -0400

    finished sorted array duplicate removal

---
+++ remove-duplicates-from-sorted-array.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        place = 0
+        prevnum = nums[0]
+        for i in range(1,len(nums)):
+            thisnum = nums[i]
+            if thisnum != prevnum:
+                place += 1
+                nums[place] = thisnum
+                prevnum = thisnum
+        return place+1
+    
